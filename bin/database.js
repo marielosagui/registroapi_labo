da225e69f22cc503fd2b55824af74da5ea529a60
Tu Nombre <you@example.com>
2019-11-04 10:10:45 -0600

    rest api

---
+++ bin/database.js
@@ -0,0 +1,16 @@
+var mongoose = require ('mongoose');
+
+let database= "registroapi";
+let port="27017";
+let host ="localhost"
+let url="mongodb://${host}:${port}/{database}";
+
+const connectDB=()=>{
+mongoose.Promise=global.Promise;
+
+mongoose
+    .connect(uri,{useNewUrlParser:true})
+    .then()=>{
+        console.log
+    }
+}
